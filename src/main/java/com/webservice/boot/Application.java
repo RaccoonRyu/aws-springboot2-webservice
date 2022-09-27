f8cde9881e24c1388608888d3d41090d58ffd4a4
RJH <rjhwind94@gmail.com>
2022-09-27 20:30:50 +0900

    Application 생성

---
+++ src/main/java/com/webservice/boot/Application.java
@@ -0,0 +1,10 @@
+package com.webservice.boot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // 해당 어노테이션을 이용해 스프링 부트의 자동 설정, 스프링 Bean 읽기/생성을 모두 자동으로 설정한다.
+public class Application { // 해당 프로젝트의 메인 클래스
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
