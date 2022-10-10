071b4501752ba10de86a1a0d0008b9554424228f
RaccoonRyu <rjhwind94@gmail.com>
2022-10-10 22:08:24 +0900

    HelloDto, HelloDtoTest 로컬 코드 merge

--- src/main/java/com/webservice/boot/dto/HelloResponseDto.java
+++ src/main/java/com/webservice/boot/dto/HelloResponseDto.java
@@ -3,19 +3,11 @@ package com.webservice.boot.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-<<<<<<< HEAD
+
 @Getter // 클래스에 선언된 모든 지역 변수의 getter 메서드를 생성
 @RequiredArgsConstructor // 선언된 모든 final 지역 변수가 포함된 생성자를 생성 (final이 아닌 지역 변수는 포함하지 않음)
-=======
-@Getter
-@RequiredArgsConstructor
->>>>>>> origin/master
 public class HelloResponseDto {
 
     private final String name;
     private final int amount;
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 }
