256a7082d71392bbe78f25e85ecaccd1dc48d075
RaccoonRyu <rjhwind94@gmail.com>
2022-10-19 22:48:08 +0900

    PostsService에 update, select 기능 추가

--- src/main/java/com/webservice/boot/service/posts/PostsService.java
+++ src/main/java/com/webservice/boot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.webservice.boot.service.posts;
 
+import com.webservice.boot.domain.posts.Posts;
 import com.webservice.boot.domain.posts.PostsRepository;
+import com.webservice.boot.dto.PostsResponseDto;
 import com.webservice.boot.dto.PostsSaveRequestDto;
+import com.webservice.boot.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,5 +24,22 @@ public class PostsService {
     @Transactional
     public Long savePosts(PostsSaveRequestDto postsSaveRequestDto) {
         return postsRepository.save(postsSaveRequestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                                     .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                                      .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
+
+        return new PostsResponseDto(entity);
     }
 }
