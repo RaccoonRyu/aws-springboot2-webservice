d3854254b634fdc63fbdd3b6a6bbfa90df18b446
RaccoonRyu <rjhwind94@gmail.com>
2022-10-23 23:23:04 +0900

    Application에 JPA Auditing 하도록 추가

--- src/main/java/com/webservice/boot/Application.java
+++ src/main/java/com/webservice/boot/Application.java
@@ -1,7 +1,9 @@
 package com.webservice.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication // 해당 어노테이션을 이용해 스프링 부트의 자동 설정, 스프링 Bean 읽기/생성을 모두 자동으로 설정한다.
 public class Application { // 해당 프로젝트의 메인 클래스
     public static void main(String[] args) {
