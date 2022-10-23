237c8e601c16691f8586731bf9c2a3c1e532fea8
RaccoonRyu <rjhwind94@gmail.com>
2022-10-23 23:24:41 +0900

    PostsApiControllerTest에 LocalDateTime 테스트 코드 추가

--- src/test/java/com/webservice/boot/web/PostsApiControllerTest.java
+++ src/test/java/com/webservice/boot/web/PostsApiControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -89,14 +90,39 @@ public class PostsApiControllerTest {
 
         HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
 
+        // when
         ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT,
                                                 requestEntity, Long.class);
 
+        // then
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+    }
+
+    @Test
+    public void BaseTimeEntityRegister() {
+
+        // given
+        LocalDateTime now = LocalDateTime.of(2022,10,23,0,0,0);
+        postsRepository.save(Posts.builder()
+                    .title("title")
+                    .content("content")
+                    .author("author")
+                    .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createdDate = "+posts.getCreatedDate()+", modifiedDate = "+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
