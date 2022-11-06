fd0381ecbc9354040bd8cf032899e8beca870f54
RaccoonRyu <rjhwind94@gmail.com>
2022-11-06 17:34:49 +0900

    PostsApiController에 삭제기능 추가

--- src/main/java/com/webservice/boot/web/PostsApiController.java
+++ src/main/java/com/webservice/boot/web/PostsApiController.java
@@ -33,4 +33,10 @@ public class PostsApiController {
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long deletePosts(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
 }
