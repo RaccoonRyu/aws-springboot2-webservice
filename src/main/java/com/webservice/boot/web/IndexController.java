15b76d9c73d56b1d2b271ef9cfd6488d8491750f
RaccoonRyu <rjhwind94@gmail.com>
2022-11-06 17:09:03 +0900

    IndexController에 전체 게시글 조회 및 게시글 수정 기능 작성

--- src/main/java/com/webservice/boot/web/IndexController.java
+++ src/main/java/com/webservice/boot/web/IndexController.java
@@ -1,18 +1,34 @@
 package com.webservice.boot.web;
 
+import com.webservice.boot.dto.PostsResponseDto;
+import com.webservice.boot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) { // Model : 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 때 사용
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
