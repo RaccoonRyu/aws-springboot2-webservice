78dfecc679352be24047965556f79d673752218b
RaccoonRyu <rjhwind94@gmail.com>
2022-11-08 22:51:20 +0900

    스프링 시큐리티 설정 class SecurityConfig 작성

---
+++ src/main/java/config/auth/SecurityConfig.java
@@ -0,0 +1,30 @@
+package config.auth;
+
+import com.webservice.boot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity //
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                    .headers().frameOptions().disable() //
+                    .and().authorizeRequests() //
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**")
+                    .permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //
+                    .anyRequest().authenticated() //
+                    .and().logout().logoutSuccessUrl("/") //
+                    .and().oauth2Login() //
+                    .userInfoEndpoint() //
+                    .userService(customOAuth2UserService); //
+    }
+
+}
