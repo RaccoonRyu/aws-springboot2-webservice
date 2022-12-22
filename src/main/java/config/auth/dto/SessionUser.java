13c02f85e77041fb03f47f9f9f70482e401b6465
RaccoonRyu <rjhwind94@gmail.com>
2022-12-22 21:52:01 +0900

    OAuthAttributes에서 User 엔티티를 생성하기 위한 SessionUser 클래스 작성

---
+++ src/main/java/config/auth/dto/SessionUser.java
@@ -0,0 +1,21 @@
+package config.auth.dto;
+
+import com.webservice.boot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+
+}
